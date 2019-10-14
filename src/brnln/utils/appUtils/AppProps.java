ede6d0ad77cc1a43baddfd2a4e06cdf33a026b7d
Brian Lin <55646920+brianlin2019@users.noreply.github.com>
2019-10-14 10:46:46 +0800

    Update AppProps.java

--- src/brnln/utils/appUtils/AppProps.java
+++ src/brnln/utils/appUtils/AppProps.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-// hi...1
+// hi...2
 public class AppProps {
 
     private final Properties myProps;
